9e064c0f64a5f7db716dc749d5e08a1f237debc0
Md-Emon-Hasan <iconiemon01@gmail.com>
2024-11-28 18:52:22 +0600

    updated

--- Falsk/test1/app.py
+++ Falsk/test1/app.py
@@ -1,21 +1,20 @@
+import joblib
 from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import render_template
-import joblib
 import numpy as np
 
 app = Flask(__name__)
 
 # load the model
-# model = joblib.load('iris_model.pkl')
-model = joblib.load('C:/Users/emon1/OneDrive/Desktop/Practice Problem/Falsk/Deploy Machine Learning Model Flask/iris_model.pkl')
+model = joblib.load('C:/Users/emon1/OneDrive/Desktop/Practice Problem/Falsk/test1/iris_model.pkl')
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
     data = [
         float(request.form['sepal_length']),
@@ -24,9 +23,10 @@ def predict():
         float(request.form['petal_width'])
     ]
     prediction = model.predict([data])
-    species    = ['setosa','versicolor','virginica']
-    result     = species[prediction[0]]
-    return render_template('index.html',prediction_text=f"Iris flower is predicted to be {result}")
+    species =  ['setosa', 'versicolor', 'virginica']
+    result = species[prediction[0]]
+    
+    return render_template('index.html',prediction_text=f'Iris flower is predicted to be {result}')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Falsk/test1/model.py
+++ Falsk/test1/model.py
@@ -1,24 +1,24 @@
-# Importing dependency
-from sklearn.datasets import load_iris
+# import dependency
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import joblib
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_iris
+import pickle
 
-# Load the iris dataset
-iris = load_iris()
+# load iris datasets
+df = load_iris()
 
 # target the data
-x = iris.data
-y = iris.target
+x = df.data
+y = df.target
 
-# split the data
+# train the data
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
 
-# train the data in RandomForesClassifier Algorithm
-model = RandomForestClassifier(n_estimators=100,ranodm_state=42)
+# set algorithm
+model = LinearRegression()
 
 # fit the model
 model.fit(x_train,y_train)
 
-# save the trained model to a file
-joblib.dump(model,'iris_model.pkl')
+# save the train model into the file
+pickle.dump(model,open('model.pkl','wb'))
